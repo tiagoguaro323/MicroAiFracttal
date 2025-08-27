830dea10f8bbf54f13d4164f979f457cf4c6655a
santiago guarin <santiago.guarin@fracttal.com>
2025-08-27 12:46:47 -0500

    ...

--- src/view/Auth/components/CheckEmail.tsx
+++ src/view/Auth/components/CheckEmail.tsx
@@ -106,9 +106,9 @@ const CheckEmail: FC<IProps> = ({
           password: auth.password,
         });
       }
-      console.log(host);
+
       setLoading(true);
-      fetch(`https://develop.fracttal.com/rpc/check_user`, {
+      fetch(`${host}/rpc/check_user`, {
         method: 'POST',
         body: JSON.stringify({ email: auth.email, password: pwd }),
         headers: {
